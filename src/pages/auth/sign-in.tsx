6a1ef5d0b429c33b156fe77cdb9e026880576bd7
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-03-06 21:53:19 -0300

    API - restaurant registration

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -17,11 +17,17 @@ const signInForm = z.object({
 type SignInFrom = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const [searchParms] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInFrom>()
+  } = useForm<SignInFrom>({
+    defaultValues: {
+      email: searchParms.get('email') ?? '',
+    },
+  })
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
