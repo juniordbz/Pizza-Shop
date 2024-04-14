1fb4c3101563f6452eaec0a47644cac6c38dc912
juniordbz <franciscojunior02@hotmail.com.br>
2024-04-14 13:00:18 -0300

    add mocks orders and orders details

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -8,6 +8,8 @@ import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount'
 import { getMonthRevenueMock } from './get-month-revenue'
+import { getOrderDetailsMock } from './get-orders-details-mock'
+import { getOrdersMock } from './get-orders-mocks'
 import { getPopularProductsMock } from './get-popular-products-mock'
 import { getProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mocks'
@@ -26,6 +28,8 @@ export const worker = setupWorker(
   updateProfileMock,
   getProfileMock,
   getManagedRestaurantMock,
+  getOrdersMock,
+  getOrderDetailsMock,
 )
 
 export async function enableMSW() {
